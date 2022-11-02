7ba3d197a9fc5523543bec41da35dcef1518c0d8
linyin <lujiashuai.777@bytedance.com>
2022-11-02 12:59:37 +0800

    后端文件上传接口，文件管理数据库

--- model/example/exa_file_upload_download.go
+++ model/example/exa_file_upload_download.go
@@ -2,14 +2,17 @@ package example
 
 import (
 	"eva/global"
+	"eva/model/system"
 )
 
 type ExaFileUploadAndDownload struct {
 	global.EVA_MODEL
-	Name string `json:"name" gorm:"comment:文件名"` // 文件名
-	Url  string `json:"url" gorm:"comment:文件地址"` // 文件地址
-	Tag  string `json:"tag" gorm:"comment:文件标签"` // 文件标签
-	Key  string `json:"key" gorm:"comment:编号"`   // 编号
+	Name      string `json:"name" gorm:"comment:文件名"` // 文件名
+	Url       string `json:"url" gorm:"comment:文件地址"` // 文件地址
+	Tag       string `json:"tag" gorm:"comment:文件标签"` // 文件标签
+	Key       string `json:"key" gorm:"comment:编号"`   // 编号
+	SysUserID int
+	SysUser   system.SysUser
 }
 
 func (ExaFileUploadAndDownload) TableName() string {
